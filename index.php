f1924567c5f536f2df6dba917e0c5843e1e85fe9
musicpop <playmusic8@gmail.com>
2014-11-07 16:47:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
 <head>
-<title>����ͬѧ¼-��Ա��¼</title>
+<title>杏林同学录-会员登录branch1</title>
 <meta http-equiv="Content-Type" content="text/html; charset=gb2312">
 <style type="text/css">
 <!--
-.deepblue10 {  font-family: "����"; font-size: 10pt; color: #3333FF; text-decoration: none}
+.deepblue10 {  font-family: "宋体"; font-size: 10pt; color: #3333FF; text-decoration: none}
 A.purple:hover {  font-size: 9pt; color: #3366FF; background-color: #99FFFF; text-decoration: none}
 A.purple:link {  font-size: 10pt; text-decoration: none}
 a:hover {  text-decoration: none}
@@ -34,30 +34,30 @@ a:visited {  text-decoration: none}
         </tr>
         <tr> 
           <td width="33%" height="17"> 
-            <div align="center">�༶��Ա��¼ </div>
+            <div align="center">班级成员登录 </div>
           </td>
           <td width="67%" height="17"> 
-            <div align="center"><a href="member/register_step1.php" class="purple">�༶��Աע��</a></div>
+            <div align="center"><a href="member/register_step1.php" class="purple">班级成员注册</a></div>
           </td>
         </tr>
         <tr>
           <td width="33%"> 
             <form method="post" action="frame.php">
-              <div align="center">�û��� 
+              <div align="center">用户名 
                 <input type="text" name="user" size="8">
                 <br>
-                ����&nbsp; 
+                密码&nbsp; 
                 <input type="password" name="psw" size="8">
                 <br>
                 <br>
-                <input type="submit" name="Submit" value="��¼">
-                <input type="reset" name="cancel" value="��д">
+                <input type="submit" name="Submit" value="登录">
+                <input type="reset" name="cancel" value="重写">
               </div>
             </form>
           </td>
           <td width="67%"> 
             <div align="center"> 
-              <p>�������룺�����Ա��Ҫ����</p>
+              <p>忘了密码：向管理员索要密码</p>
             </div>
           </td>
         </tr>
